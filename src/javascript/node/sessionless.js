2857e02e019dca5c0be9b7c07e4cd933bf0b1ef5
zach-planet-nine <zach@planetnine.app>
2024-02-07 23:07:42 -0800

    rename

--- src/javascript/node/sessionless.js
+++ src/javascript/node/sessionless.js
@@ -6,24 +6,29 @@ import { getRandomBytesSync } from "ethereum-cryptography/random.js";
 
 let getKeysFromDisk;
 
+const AsyncFunction = (async () => {}).constructor;
+
 const generateKeys = (saveKeys, getKeys) => {
   if(!saveKeys || !getKeys) {
     return console.warn(`Since this can be run on any machine with node, there is no default secure storage. You will need to provide a saveKeys and getKeys function`);
   }
   const privateKey = secp256k1.utils.randomPrivateKey();
   const publicKey = secp256k1.getPublicKey(privateKey);
-  saveKeys && saveKeys({
+  saveKeys && (saveKeys instanceof AsyncFunction ? await saveKeys({
     privateKey,
     publicKey
-  });
+  }) : saveKeys({
+    privateKey,
+    publicKey
+  }));
   getKeysFromDisk = getKeys;
 };
 
-const getKeys = () => {
+const getKeys = async () => {
   if(!getKeysFromDisk) {
     return console.error(`Since this can be run on any machine with node, there is no default secure storage. You will need to have your own getKeys function.`);
   } else {
-    return getKeysFromDisk();
+    return getKeysFromDisk instanceof AsyncFunction ? await getKeysFromDisk() : getKeysFromDisk();
   }
 };
 
