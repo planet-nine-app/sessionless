b25b5bc1b429d00d0cb11aadbcaa199b7f49c152
zach-planet-nine <zach@planetnine.app>
2024-02-17 18:52:34 -0800

    sessionless update for cjs

--- src/javascript/node/sessionless.cjs
+++ src/javascript/node/sessionless.cjs
@@ -1,8 +1,9 @@
-import { secp256k1 } from 'ethereum-cryptography/secp256k1';
-import { keccak256 } from "ethereum-cryptography/keccak.js";
-import { utf8ToBytes } from "ethereum-cryptography/utils.js";
-import { bytesToHex } from "ethereum-cryptography/utils.js";
-import { getRandomBytesSync } from "ethereum-cryptography/random.js";
+'use strict';
+
+var secp256k1 = require('ethereum-cryptography/secp256k1');
+var keccak_js = require('ethereum-cryptography/keccak.js');
+var utils_js = require('ethereum-cryptography/utils.js');
+var random_js = require('ethereum-cryptography/random.js');
 
 let getKeysFromDisk;
 
@@ -12,8 +13,8 @@ const generateKeys = async (saveKeys, getKeys) => {
   if(!saveKeys || !getKeys) {
     return console.warn(`Since this can be run on any machine with node, there is no default secure storage. You will need to provide a saveKeys and getKeys function`);
   }
-  const privateKey = secp256k1.utils.randomPrivateKey();
-  const publicKey = secp256k1.getPublicKey(privateKey);
+  const privateKey = secp256k1.secp256k1.utils.randomPrivateKey();
+  const publicKey = secp256k1.secp256k1.getPublicKey(privateKey);
   saveKeys && (saveKeys instanceof AsyncFunction ? await saveKeys({
     privateKey,
     publicKey
@@ -34,19 +35,19 @@ const getKeys = async () => {
 
 const sign = (message) => {
   const { privateKey } = getKeys();
-  const messageHash = keccak256(utf8ToBytes(message));
-  return secp256k1.sign(messageHash, privateKey);
+  const messageHash = keccak_js.keccak256(utils_js.utf8ToBytes(message));
+  return secp256k1.secp256k1.sign(messageHash, privateKey);
 };
 
 const verifySignature = (signature, message, publicKey) => {
-  const signatureHexString = signature.r + signature.s;
-  const messageHash = keccak256(utf8ToBytes(message.slice(0,32)));
+  signature.r + signature.s;
+  const messageHash = keccak_js.keccak256(utils_js.utf8ToBytes(message.slice(0,32)));
   let hex = signature.r;
   if (hex.length % 2) { hex = '0' + hex; }
 
   var bn = BigInt('0x' + hex);
 
-  var d = bn.toString(10);
+  bn.toString(10);
   let hex2 = signature.s;
   if (hex2.length % 2) { hex2 = '0' + hex2; }
 
@@ -56,12 +57,12 @@ const verifySignature = (signature, message, publicKey) => {
     r: bn,
     s: bn2
   };
-  const res = secp256k1.verify(signature2, messageHash, publicKey);
+  const res = secp256k1.secp256k1.verify(signature2, messageHash, publicKey);
   return res;
 };
 
 const generateUUID = () => {
-  return  bytesToHex(getRandomBytesSync(32));
+  return  utils_js.bytesToHex(random_js.getRandomBytesSync(32));
 };
 
 const sessionless = {
@@ -72,4 +73,4 @@ const sessionless = {
   generateUUID
 };
 
-export default sessionless;
+module.exports = sessionless;
