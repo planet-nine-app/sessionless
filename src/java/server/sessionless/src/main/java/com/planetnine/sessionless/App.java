1f4ac38450dfb58b012280d3955717e54a87181d
zach-planet-nine <zach@planetnine.app>
2024-02-19 14:51:29 -0800

    starting on java
    Former-commit-id: c62f63284d911ef22ea9214060010f81492f56be

---
+++ src/java/server/sessionless/src/main/java/com/planetnine/sessionless/App.java
@@ -0,0 +1,11 @@
+package com.planetnine.sessionless;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
+
---
+++ src/java/server/sessionless/src/main/java/com/planetnine/sessionless/GetKeys.java
@@ -0,0 +1,5 @@
+package com.planetnine.sessionless;
+
+public interface GetKeys {
+     Keys getKeys();
+};
