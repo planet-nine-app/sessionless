a2cc1679af29434f4ca5ae3d19d1e3be516976e8
zach-planet-nine <zach@planetnine.app>
2024-02-19 23:28:12 -0800

    java server and example

--- src/java/server/sessionless/src/main/java/com/planetnine/sessionless/Sessionless.java
+++ src/java/server/sessionless/src/main/java/com/planetnine/sessionless/Sessionless.java
@@ -2,11 +2,11 @@ package com.planetnine.sessionless;
 import java.lang.reflect.Method;
 
 public interface Sessionless {
-    void generateKeys(Method setKeys, Method getKeys);
+    public void generateKeys(Method setKeys, Method getKeys);
 
-    String sign(String message);
+    public String sign(String message);
 
-    Boolean verifySignature(String signature, String message, String publicKey);
+    public Boolean verifySignature(String signature, String message, String publicKey);
 
-    String generateUUID(); 
+    public String generateUUID(); 
 };
---
+++ src/java/spring/initial/src/main/java/com/example/restservice/User.java
@@ -0,0 +1,3 @@
+package com.example.restservice;
+
+public record User(String uuid, String welcomeMessage) { }
