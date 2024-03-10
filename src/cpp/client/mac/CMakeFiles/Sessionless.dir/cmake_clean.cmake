ce754c670b6bbf42646906e82b30eecaae112385
zach-planet-nine <zach@planetnine.app>
2024-03-09 22:55:02 -0800

    c++
    Former-commit-id: c4544f970a85eee03b6067d061aae7b61fc5c0b1

---
+++ src/cpp/client/mac/CMakeFiles/Sessionless.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sessionless.dir/main.cpp.o"
+  "CMakeFiles/Sessionless.dir/main.cpp.o.d"
+  "CMakeFiles/Sessionless.dir/sessionless.cpp.o"
+  "CMakeFiles/Sessionless.dir/sessionless.cpp.o.d"
+  "Sessionless"
+  "Sessionless.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sessionless.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
