0ecaa2862112d06620241fecb4cc514661c4f550
zach-planet-nine <zach@planetnine.app>
2024-03-28 18:58:14 -0700

    removing ios files which should be auto removed
    Former-commit-id: acde25b351144a27a0ce3e9ecf081181a23097dc

---
+++ src/javascript/example/client-expo/ios/Pods/Target Support Files/ExpoCrypto/ExpoCrypto-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ExpoCrypto : NSObject
+@end
+@implementation PodsDummy_ExpoCrypto
+@end
---
+++ src/javascript/example/client-expo/ios/Pods/Target Support Files/ExpoSecureStore/ExpoSecureStore-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ExpoSecureStore : NSObject
+@end
+@implementation PodsDummy_ExpoSecureStore
+@end
