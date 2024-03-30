ce1938a17f4a0e39f809cc74cdd556f67161377e
FssAy <49795742+FssAy@users.noreply.github.com>
2024-03-30 17:57:58 +0100

    Finalize the Rust library

---
+++ src/rust/crate/src/hex/error.rs
@@ -0,0 +1,27 @@
+use std::fmt::{Debug, Display, Formatter};
+use super::*;
+
+pub use hex_core::FromHexError;
+
+#[derive(Debug)]
+pub enum HexError<E: std::error::Error> {
+    HexFormat(FromHexError),
+    Other(E),
+}
+
+impl<E: std::error::Error> From<FromHexError> for HexError<E> {
+    fn from(err: FromHexError) -> Self {
+        Self::HexFormat(err)
+    }
+}
+
+impl<E: std::error::Error> Display for HexError<E> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::HexFormat(hex_err) => Display::fmt(hex_err, f),
+            Self::Other(err) => Display::fmt(err, f),
+        }
+    }
+}
+
+impl<E: std::error::Error> std::error::Error for HexError<E> {}
