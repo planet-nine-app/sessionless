c59ac311ea2885b1dbcac15fe85f59dcae3a0c09
Daniel-J-Mason <daniel.j.msn@gmail.com>
2024-03-30 11:26:24 -0700

    add database configs to tie application properties to build

---
+++ src/java/spring/src/main/java/app/planentnine/springsessionless/adapter/config/DataSourceConfig.java
@@ -0,0 +1,41 @@
+package app.planentnine.springsessionless.adapter.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    
+    @Bean("dataSource")
+    public DataSource getDataSource(@Qualifier("dataSourceProperties") DataSourceProperties dataSourceProperties) {
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(dataSourceProperties.getDriverClassName());
+        dataSourceBuilder.url(dataSourceProperties.getUrl());
+        dataSourceBuilder.username(dataSourceProperties.getUsername());
+        dataSourceBuilder.password(dataSourceProperties.getPassword());
+        return dataSourceBuilder.build();
+    }
+    
+    @Configuration
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Data
+    public static class DataSourceProperties {
+        
+        private String url;
+        private String username;
+        private String password;
+        private String driverClassName;
+        
+        @Bean("dataSourceProperties")
+        public DataSourceProperties dataSourceProperties() {
+            return this;
+        }
+    }
+}
---
+++ src/java/spring/src/main/java/app/planentnine/springsessionless/adapter/config/MybatisConfig.java
@@ -0,0 +1,32 @@
+package app.planentnine.springsessionless.adapter.config;
+
+import app.planentnine.springsessionless.adapter.persistence.mybatis.PostgresUserRepository;
+import app.planentnine.springsessionless.application.util.UuidTypeHandler;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperFactoryBean;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class MybatisConfig {
+    
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("dataSource") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource);
+        sessionFactoryBean.setTypeHandlers(new UuidTypeHandler());
+        
+        return sessionFactoryBean.getObject();
+    }
+    
+    @Bean
+    public MapperFactoryBean<PostgresUserRepository> postgresUserRepository (SqlSessionFactory sqlSessionFactory) {
+        MapperFactoryBean<PostgresUserRepository> factoryBean = new MapperFactoryBean<>(PostgresUserRepository.class);
+        factoryBean.setSqlSessionFactory(sqlSessionFactory);
+        return factoryBean;
+    }
+}
