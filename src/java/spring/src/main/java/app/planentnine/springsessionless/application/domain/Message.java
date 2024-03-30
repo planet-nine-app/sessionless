79bb3f8da0caca56121e8aed45d0610363918bba
Daniel-J-Mason <daniel.j.msn@gmail.com>
2024-03-30 11:27:44 -0700

    remove todo

--- src/java/spring/src/main/java/app/planentnine/springsessionless/application/domain/Message.java
+++ src/java/spring/src/main/java/app/planentnine/springsessionless/application/domain/Message.java
@@ -3,6 +3,5 @@ package app.planentnine.springsessionless.application.domain;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-//TODO convert signature to appropriate type
 public record Message(UUID userUuid, String content, String[] signature, LocalDateTime timestamp) {
 }
