1ec10af122dec9091f4f52524cfba100f302ac61
Daniel-J-Mason <daniel.j.msn@gmail.com>
2024-03-29 19:01:32 -0700

    update message class to clearer variable name

--- src/java/spring/src/main/java/app/planentnine/springsessionless/application/domain/Message.java
+++ src/java/spring/src/main/java/app/planentnine/springsessionless/application/domain/Message.java
@@ -1,9 +1,8 @@
 package app.planentnine.springsessionless.application.domain;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
 //TODO convert signature to appropriate type
-public record Message(UUID userUuid, String message, List<String> signature, LocalDateTime timestamp) {
+public record Message(UUID userUuid, String content, String[] signature, LocalDateTime timestamp) {
 }
