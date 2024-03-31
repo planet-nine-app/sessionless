d9c98639f44494091eb0fc14409db1bc3bcf5bd5
Galacticai <24789792+Galacticai@users.noreply.github.com>
2024-03-31 10:04:45 +0300

    update: pass `KeyPair` instead of separate public/private parameters
    Former-commit-id: c78b4f8dfd64b2a6ec85ba8488fb38ef115a3e1a

--- src/kotlin/src/main/kotlin/com/planetnine/sessionless/SimpleKeyPair.kt
+++ src/kotlin/src/main/kotlin/com/planetnine/sessionless/SimpleKeyPair.kt
@@ -1,17 +1,16 @@
 package com.planetnine.sessionless
 
-import java.security.PrivateKey
-import java.security.PublicKey
+import java.security.KeyPair
 import java.util.Base64
 
 /** Simplified key pair (as [String]s)*/
 data class SimpleKeyPair(val privateKey: String, val publicKey: String) {
     companion object {
-        fun from(privateKey: PrivateKey, publicKey: PublicKey): SimpleKeyPair {
+        fun from(pair: KeyPair): SimpleKeyPair {
             val encoder = Base64.getEncoder()
             return SimpleKeyPair(
-                encoder.encodeToString(privateKey.encoded),
-                encoder.encodeToString(publicKey.encoded)
+                encoder.encodeToString(pair.private.encoded),
+                encoder.encodeToString(pair.public.encoded)
             )
         }
     }
