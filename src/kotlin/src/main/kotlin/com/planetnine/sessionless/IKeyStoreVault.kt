640bddd3d465b0e1c5e60a8604131c9b2371c038
Galacticai <24789792+Galacticai@users.noreply.github.com>
2024-03-31 10:04:01 +0300

    move: `IKeyStoreVault` default implementation from `IVault` to `IKeyStoreVault` body
    Former-commit-id: 1407de4083995be7d94ecce606d9d83624ecad96

--- src/kotlin/src/main/kotlin/com/planetnine/sessionless/IKeyStoreVault.kt
+++ src/kotlin/src/main/kotlin/com/planetnine/sessionless/IKeyStoreVault.kt
@@ -2,10 +2,13 @@ package com.planetnine.sessionless
 
 import java.io.ByteArrayInputStream
 import java.security.KeyPair
+import java.security.KeyStore
+import java.security.PrivateKey
 import java.security.cert.Certificate
 import java.security.cert.CertificateFactory
 
-abstract class IKeyStoreVault : IVault {
+/** Abstract definition of [IVault] which uses [KeyStore] */
+abstract class IKeyStoreVault(protected val keyStore: KeyStore) : IVault {
     @Throws(java.security.cert.CertificateException::class)
     fun save(
         alias: String,
@@ -29,4 +32,34 @@ abstract class IKeyStoreVault : IVault {
     )
 
     abstract fun get(alias: String, password: CharArray? = null): KeyPair
+
+
+    /** Default implementation of [IKeyStoreVault] */
+    class Default(keyStore: KeyStore) : IKeyStoreVault(keyStore) {
+        @Throws(java.security.KeyStoreException::class)
+        override fun save(
+            alias: String,
+            pair: KeyPair,
+            password: CharArray?,
+            certificate: Certificate
+        ) {
+            keyStore.setKeyEntry(
+                alias,
+                pair.private,
+                password,
+                arrayOf(certificate)
+            )
+        }
+
+        @Throws(
+            java.security.KeyStoreException::class,
+            java.security.NoSuchAlgorithmException::class,
+            java.security.UnrecoverableKeyException::class
+        )
+        override fun get(alias: String, password: CharArray?): KeyPair {
+            val privateKey = keyStore.getKey(alias, password) as PrivateKey
+            val publicKey = keyStore.getCertificate(alias).publicKey
+            return KeyPair(publicKey, privateKey)
+        }
+    }
 }
