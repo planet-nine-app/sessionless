7bf3d813f9291982b1495e7c9ad07f79e2364fc4
FssAy <49795742+FssAy@users.noreply.github.com>
2024-03-31 22:16:18 +0200

    Fix tampering with the payload in the Rust web example

--- src/rust/examples/web-server/src/request/objects.rs
+++ src/rust/examples/web-server/src/request/objects.rs
@@ -4,8 +4,6 @@ pub struct PayloadRegister {
     pub public_key: String,
     pub timestamp: String,
     pub entered_text: String,
-    #[serde(skip_serializing)]
-    pub signature: String,
 }
 
 #[derive(Deserialize, Serialize)]
@@ -13,8 +11,5 @@ pub struct PayloadRegister {
 pub struct PayloadCoolStuff {
     pub timestamp: String,
     pub coolness: String,
-    #[serde(skip_serializing)]
     pub uuid: String,
-    #[serde(skip_serializing)]
-    pub signature: String,
 }
