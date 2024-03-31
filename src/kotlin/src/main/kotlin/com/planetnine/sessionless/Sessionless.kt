f3429c2312ced92f7a0d9100ee87ae9b6c8de001
Galacticai <24789792+Galacticai@users.noreply.github.com>
2024-04-01 00:17:14 +0300

    feat: implementation of `ISessionless`
    - generateKeys
    - getKeys
    - sign
    - verifySignature
    - generateUUID
    - (TODO: The rest)

--- src/kotlin/src/main/kotlin/com/planetnine/sessionless/Sessionless.kt
+++ src/kotlin/src/main/kotlin/com/planetnine/sessionless/Sessionless.kt
@@ -1,101 +1,93 @@
 package com.planetnine.sessionless
 
-import org.springframework.beans.factory.annotation.Autowired
+import com.planetnine.sessionless.Sessionless.WithCustomVault
+import com.planetnine.sessionless.Sessionless.WithKeyStore
+import java.security.KeyFactory
 import java.security.KeyPair
-import java.security.KeyPairGenerator
-import java.security.KeyStore
-import java.security.SecureRandom
+import java.security.PrivateKey
+import java.security.Signature
+import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.X509EncodedKeySpec
+import java.util.Base64
 import java.util.UUID
 
-class Sessionless(
-    private val keyFactoryInfo: KeyFactoryInfo = KeyFactoryInfo(),
-    private var customKeysProvider: IKeyPairStore? = null
-) : ISessionless {
-
-    @Autowired
-    private lateinit var keyStore: KeyStore
-
-    private fun newKeyPair(): KeyPair {
-        val gen = KeyPairGenerator.getInstance(
-            keyFactoryInfo.algorithm,
-            keyFactoryInfo.provider
-        )
-        val random = SecureRandom.getInstance(
-            keyFactoryInfo.randomAlgorithm,
-            keyFactoryInfo.provider
-        )
-        gen.initialize(Defaults.KEY_SIZE, random)
-        return gen.generateKeyPair()
-    }
-
-    override fun generateKeys(
-        saveKeys: (keys: SimpleKeyPair) -> Unit,
-        getKeys: () -> SimpleKeyPair
-    ) {
-        val provider = object : IKeyPairStore {
-            override fun save(keys: SimpleKeyPair) = saveKeys(keys)
-            override fun get() = getKeys()
+/** [Sessionless] implementation (sealed)
+ * @see WithKeyStore
+ * @see WithCustomVault */
+sealed class Sessionless(override val vault: IVault) : ISessionless {
+    /** [Sessionless.WithKeyStore] implementation */
+    class WithKeyStore(override val vault: IKeyStoreVault) :
+        Sessionless(vault),
+        ISessionless.WithKeyStore {
+        override fun generateKeys(info: KeyInfo.ForKeyStore): KeyPair {
+            val pair = info.generateKeyPair()
+            vault.save(
+                info.accessInfo.alias,
+                pair,
+                info.accessInfo.password,
+                info.certificateFactory
+            )
+            return pair
         }
-        customKeysProvider = provider
-        generateKeys()
-    }
 
+        override fun getKeys(accessInfo: KeyInfo.ForKeyStore.AccessInfo): KeyPair =
+            vault.get(accessInfo.alias, accessInfo.password)
 
-    /** Generates a private/public key pair and stores it in the platform's secure storage.
-     * - ⚠️ with a custom [store] ([IKeyPairStore]).
-     * @param store provides a custom way to get/save keys. */
-    fun generateKeys(store: IKeyPairStore) = generateKeys(store::save, store::get)
-
-    /** Generates a private/public key pair and stores it in the platform's secure storage.
-     * - Uses platform's default secure storage ([java.security.KeyStore]) */
-    fun generateKeys() {
-        val pair = newKeyPair()
-        val provider = customKeysProvider
-        if (provider == null) {
-            TODO("Not yet implemented")
-            val platformStore = object : IKeyPairStore {
-                override fun save(keys: SimpleKeyPair) {
-//                    keyStore.setKeyEntry(
-//                        keyFactoryInfo.alias,
-//                        pair.private,
-//                        keyFactoryInfo.password?.toCharArray(),
-//                        pair.public
-//                    )
-                    TODO()
-                }
-
-                override fun get(): SimpleKeyPair {
-//                    val entry = keyStore.getEntry(keyFactoryInfo.alias, null)
-//                            as? KeyStore.PrivateKeyEntry
-                    TODO()
-                }
-            }
-            customKeysProvider = platformStore
-        } else {
-            val simpleKeyPair = SimpleKeyPair.from(pair.private, pair.public)
-            provider.save(simpleKeyPair)
+        override fun sign(message: String, keyAccessInfo: KeyInfo.ForKeyStore.AccessInfo): String {
+            val privateKey = getKeys(keyAccessInfo).private
+            return sign(message, privateKey)
         }
     }
 
-    override fun getKeys(): SimpleKeyPair {
-        val provider = customKeysProvider
-        if (provider != null) return provider.get()
+    /** [Sessionless.WithCustomVault] implementation */
+    class WithCustomVault(override val vault: ICustomVault) :
+        Sessionless(vault),
+        ISessionless.WithCustomVault {
+        override fun generateKeys(info: KeyInfo.ForCustom): SimpleKeyPair {
+            val pair = info.generateKeyPair()
+            val simple = SimpleKeyPair.from(pair)
+            vault.save(simple)
+            return simple
+        }
 
-        TODO("Not yet implemented")
+        override fun getKeys(): SimpleKeyPair = vault.get()
+
+        override fun sign(message: String): String {
+            val privateString = getKeys().privateKey
+            val privateBytes = Base64.getDecoder().decode(privateString)
+            val privateSpec = PKCS8EncodedKeySpec(privateBytes)
+            val privateKey = KeyFactory.getInstance(ISessionless.KEY_ALGORITHM)
+                .generatePrivate(privateSpec)
+            return sign(message, privateKey)
+        }
     }
 
-    override fun sign(message: String): String {
-        TODO("Not yet implemented")
+    override fun sign(message: String, privateKey: PrivateKey): String {
+        val signature = Signature.getInstance(ISessionless.SIGNATURE_ALGORITHM)
+        signature.initSign(privateKey)
+        signature.update(message.toByteArray())
+        val bytes = signature.sign()
+        return String(bytes, Charsets.UTF_8)
     }
 
     override fun verifySignature(message: String, signature: String, publicKey: String): Boolean {
-        TODO("Not yet implemented")
+        val kf = KeyFactory.getInstance(ISessionless.KEY_ALGORITHM)
+        val decoder = Base64.getDecoder()
+        val publicKeyBytes = decoder.decode(publicKey)
+        val publicKeySpec = X509EncodedKeySpec(publicKeyBytes)
+        val pubKey = kf.generatePublic(publicKeySpec)
+
+        val signatureObj = Signature.getInstance(ISessionless.SIGNATURE_ALGORITHM)
+        signatureObj.initVerify(pubKey)
+        signatureObj.update(message.toByteArray())
+
+        val signatureBytes = decoder.decode(signature)
+        return signatureObj.verify(signatureBytes)
     }
 
-    override fun generateUUID(): String =
-        UUID.randomUUID().toString()
+    override fun generateUUID(): String = UUID.randomUUID().toString()
 
-
+    //TODO: implement the rest
     override fun associateKeys(
         associationMessage: String,
         primarySignature: String,
@@ -109,29 +101,4 @@ class Sessionless(
         TODO("Not yet implemented")
     }
 
-    object Defaults {
-        const val KEY_ALIAS = "sessionless"
-        const val KEY_PROVIDER = "sessionless"
-        const val KEY_ALGORITHM = "RSA"
-        const val KEY_SIZE = 1024
-        const val RANDOM_ALGORITHM = "SHA1PRNG"
-    }
-
-    /** Information about the creation of a key pair
-     * - Default values are defined in [Defaults]
-     * @param alias Key alias (used in storage)
-     * @param password password securing the key pair
-     * @param algorithm algorithm used to generate the key pair
-     * @param provider provider used to generate the key pair
-     * @param size size in bits
-     * @param randomAlgorithm algorithm used to generate the secure random */
-    data class KeyFactoryInfo(
-        val alias: String = Defaults.KEY_ALIAS,
-        val password: String? = null,
-        val algorithm: String = Defaults.KEY_ALGORITHM,
-        val provider: String = Defaults.KEY_PROVIDER,
-        val size: Int = Defaults.KEY_SIZE,
-        val randomAlgorithm: String = Defaults.RANDOM_ALGORITHM,
-    )
 }
-
