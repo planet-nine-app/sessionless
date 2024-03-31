933e751e304f4dae33b55ec8800797133cc6ccb8
Galacticai <24789792+Galacticai@users.noreply.github.com>
2024-04-01 00:13:51 +0300

    feat: dedicated interfaces for scenes with `KeyStore` (using `IKeyStoreVault`) and without (using `ICustomVault`)

--- src/kotlin/src/main/kotlin/com/planetnine/sessionless/ISessionless.kt
+++ src/kotlin/src/main/kotlin/com/planetnine/sessionless/ISessionless.kt
@@ -1,23 +1,57 @@
 package com.planetnine.sessionless
 
+import java.security.KeyPair
+import java.security.PrivateKey
+
 interface ISessionless {
     /** The way to store and retrieve key pairs */
     val vault: IVault
 
-    /** Generates a private/public key pair and stores it.
-     * - Uses [vault] if available
-     * - Otherwise it will use [java.security.KeyStore] */
-    fun generateKeys()
+    interface WithKeyStore : ISessionless {
+        override val vault: IKeyStoreVault
 
-    /** Retrieves keys from secure storage.
-     * - This should use [vault] if available, otherwise [java.security.KeyStore] as the default storage
-     * @return The retrieved keys object. */
-    fun getKeys(): SimpleKeyPair
+        /** Generates a private/public key pair and stores it.
+         * - Uses [vault] if available
+         * - Otherwise it will use [java.security.KeyStore]
+         * @param info info about key pair generation */
+        fun generateKeys(info: KeyInfo.ForKeyStore): KeyPair
 
-    /** Signs a message with the user's private key.
+        /** Retrieves keys from secure storage.
+         * - This should use [vault] if available, otherwise [java.security.KeyStore] as the default storage
+         * @return The retrieved keys object. */
+        fun getKeys(accessInfo: KeyInfo.ForKeyStore.AccessInfo): KeyPair
+
+        /** Signs a [message] with the user's stored private key (from [vault]).
+         * @param message The message to be signed.
+         * @param keyAccessInfo Info about accessing the key
+         * @return Signature as a [String]. */
+        fun sign(message: String, keyAccessInfo: KeyInfo.ForKeyStore.AccessInfo): String
+    }
+
+    interface WithCustomVault : ISessionless {
+        override val vault: ICustomVault
+
+        /** Generates a private/public key pair and stores it.
+         * - Uses [vault] if available
+         * - Otherwise it will use [java.security.KeyStore]
+         * @param info info about key pair generation */
+        fun generateKeys(info: KeyInfo.ForCustom): SimpleKeyPair
+
+        /** Retrieves keys from secure storage.
+         * - This should use [vault] if available, otherwise [java.security.KeyStore] as the default storage
+         * @return The retrieved keys object. */
+        fun getKeys(): SimpleKeyPair
+
+        /** Signs a [message] with the user's stored private key (from [vault]).
+         * @param message The message to be signed.
+         * @return Signature as a [String]. */
+        fun sign(message: String): String
+    }
+
+    /** Signs a [message] using the provided [privateKey].
      * @param message The message to be signed.
      * @return Signature as a [String]. */
-    fun sign(message: String): String
+    fun sign(message: String, privateKey: PrivateKey): String
 
     /** Verifies a given signature with a public key.
      * @param message The message that was signed earlier (ideally signed with [sign]).
@@ -48,5 +82,14 @@ interface ISessionless {
      * @param signature Signature using user's private key.
      * @param publicKey Gateway's public key. */
     fun revokeKey(message: String, signature: String, publicKey: String)
+
+    companion object {
+        const val KEY_PROVIDER = "sessionless"
+        const val KEY_ALGORITHM = "RSA"
+        const val SIGNATURE_ALGORITHM = "SHA256withRSA"
+        const val KEY_SIZE = 1024
+        const val RANDOM_ALGORITHM = "SHA1PRNG"
+        const val CERTIFICATE_TYPE = "X.509"
+    }
 }
 
