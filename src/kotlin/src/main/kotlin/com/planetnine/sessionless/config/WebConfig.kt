6b99b3f37be45d7c6803270435c95601a0cf6675
Galacticai <24789792+Galacticai@users.noreply.github.com>
2024-04-01 07:41:18 +0300

    feat: cors config
    Former-commit-id: 767188479fb3832cd76575a021fdcd2b5651bbd3

---
+++ src/kotlin/src/main/kotlin/com/planetnine/sessionless/config/WebConfig.kt
@@ -0,0 +1,21 @@
+package com.planetnine.sessionless.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:8080")
+                    .allowedMethods("POST", "PUT", "GET", "OPTIONS", "HEAD")
+                    .allowCredentials(true)
+            }
+        }
+    }
+}
