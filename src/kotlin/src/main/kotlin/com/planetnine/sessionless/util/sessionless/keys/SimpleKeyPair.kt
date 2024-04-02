835028865ca0bc7a617c31299a8691c6aba7d374
Galacticai <24789792+Galacticai@users.noreply.github.com>
2024-04-02 05:33:03 +0300

    feat: KeyStoreConfig for spring
    Former-commit-id: c2823dd13b4727ec4d7e65db5b433438da0aee15

--- src/kotlin/src/main/kotlin/com/planetnine/sessionless/util/sessionless/keys/SimpleKeyPair.kt
+++ src/kotlin/src/main/kotlin/com/planetnine/sessionless/util/sessionless/keys/SimpleKeyPair.kt
@@ -1,17 +1,4 @@
 package com.planetnine.sessionless.util.sessionless.keys
 
-import java.security.KeyPair
-import java.util.Base64
-
-/** Simplified key pair (as [String]s)*/
-data class SimpleKeyPair(val privateKey: String, val publicKey: String) {
-    companion object {
-        fun from(pair: KeyPair): SimpleKeyPair {
-            val encoder = Base64.getEncoder()
-            return SimpleKeyPair(
-                encoder.encodeToString(pair.private.encoded),
-                encoder.encodeToString(pair.public.encoded)
-            )
-        }
-    }
-}
+/** Simplified key pair type, comprised of [String]s */
+data class SimpleKeyPair(val privateKey: String, val publicKey: String)
