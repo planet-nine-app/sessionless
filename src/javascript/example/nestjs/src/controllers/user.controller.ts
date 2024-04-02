2e393300bbd76a8d30fca3c229892bc66fb2cc64
omaneh <122321976+omaneh@users.noreply.github.com>
2024-04-02 14:48:10 -0400

    Added validation route
    Former-commit-id: ddb84efee66b48eda5849f00846b3def3a21c368

--- src/javascript/example/nestjs/src/controllers/user.controller.ts
+++ src/javascript/example/nestjs/src/controllers/user.controller.ts
@@ -1,37 +1,41 @@
-import { Controller, Get, Post, Body, HttpException, HttpStatus } from "@nestjs/common";
+import {
+  Controller,
+  Post,
+  Body,
+  Res
+} from "@nestjs/common";
 import { CreateUserDto } from "../dtos";
 import sessionless from "sessionless-node";
 import { UserRepository } from "../repositories";
+import { Response } from 'express';
 
 @Controller("/user")
 export class UserController {
   @Post("/register")
-  async registerUser(@Body() payload: CreateUserDto) {
+  async registerUser(@Body() payload: CreateUserDto, @Res() res: Response) {
     try {
       const signature = payload.signature;
-    const publicKey = payload.publicKey;
+      const publicKey = payload.publicKey;
 
-    const message = JSON.stringify({
-      publicKey,
-      content: payload.content,
-      timestamp: payload.timestamp,
-    });
+      const message = JSON.stringify({
+        publicKey,
+        content: payload.content,
+        timestamp: payload.timestamp,
+      });
 
-    if (sessionless.verifySignature(signature, message, publicKey)) {
-      const uuid = sessionless.generateUUID()
-      await UserRepository.saveUser(uuid, publicKey)
-      const user = {
-        uuid,
-        welcomeMessage: 'Welcome to this example!'
+      if (sessionless.verifySignature(signature, message, publicKey)) {
+        const uuid = sessionless.generateUUID();
+        await UserRepository.saveUser(uuid, publicKey);
+        const user = {
+          uuid,
+          welcomeMessage: "Welcome to this example!",
+        };
+        res.status(201).send(JSON.stringify(user));
+      } else {
+        throw new Error("Something went wrong. Please try again later.");
       }
-      return JSON.stringify(user)
-    }
-    else {
-      throw new Error('Something went wrong. Please try again later.')
-    }
-    }
-    catch(error) {
-      throw new HttpException(`Error: ${error}`, HttpStatus.BAD_REQUEST)
+    } catch (error) {
+      res.status(400).send(`Error: ${error}`);
     }
   }
 }
--- src/javascript/example/nestjs/src/dtos/index.ts
+++ src/javascript/example/nestjs/src/dtos/index.ts
@@ -1,1 +1,3 @@
 export * from './user.dto'
+export * from './shared.dto'
+export * from './validate.dto'
