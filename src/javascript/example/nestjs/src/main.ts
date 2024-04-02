4dfd1a6c8b4a09426cfcb1c3da4c62d38b9a86fd
omaneh <122321976+omaneh@users.noreply.github.com>
2024-04-02 15:33:39 -0400

    Small tweaks to formatting
    Former-commit-id: 852dbcbe46f565455bad85ff18b8183c8d59d0ca

--- src/javascript/example/nestjs/src/main.ts
+++ src/javascript/example/nestjs/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
-}
+} 
 
 // Start server
 bootstrap();
