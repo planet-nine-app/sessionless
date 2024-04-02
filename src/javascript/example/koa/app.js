476796db1507856f06d92b4ee34854413ca6f2da
omaneh <122321976+omaneh@users.noreply.github.com>
2024-04-02 10:28:43 -0400

    Fixed lint issues within app.js
    Former-commit-id: df879fe9ee4eb51bf3969c9edaa75c12cb16d138

--- src/javascript/example/koa/app.js
+++ src/javascript/example/koa/app.js
@@ -1,130 +1,130 @@
-import Koa from "koa";
-import json from "koa-json";
-import KoaRouter from "koa-router";
-import bodyParser from "koa-bodyparser";
-import sessionless from "sessionless-node";
-import fs from "fs/promises";
+import Koa from 'koa'
+import json from 'koa-json'
+import KoaRouter from 'koa-router'
+import bodyParser from 'koa-bodyparser'
+import sessionless from 'sessionless-node'
+import fs from 'fs/promises'
 
-const app = new Koa();
-const router = new KoaRouter();
+const app = new Koa()
+const router = new KoaRouter()
 
-//JSON Prettier Middleware
-app.use(json());
+// JSON Prettier Middleware
+app.use(json())
 
-//Bodypaser Middleware
-app.use(bodyParser({ enableTypes: ["json", "text", "form", "xml"] }));
+// Bodypaser Middleware
+app.use(bodyParser({ enableTypes: ['json', 'text', 'form', 'xml'] }))
 
-//Router Middleware
-app.use(router.routes()).use(router.allowedMethods());
+// Router Middleware
+app.use(router.routes()).use(router.allowedMethods())
 
-//Helper methods
+// Helper methods
 const getUsers = async () => {
-  const usersString = await fs.readFile("./users.json");
-  const users = JSON.parse(usersString);
-  return users;
-};
+  const usersString = await fs.readFile('./users.json')
+  const users = JSON.parse(usersString)
+  return users
+}
 
 const saveUser = async (uuid, publicKey) => {
-  let usersJson = await getUsers();
-  let users = usersJson["users"];
-  users.push({ uuid, publicKey });
-  await fs.writeFile("./users.json", JSON.stringify({ users: users }), "utf8");
-};
+  const usersJson = await getUsers()
+  const users = usersJson.users
+  users.push({ uuid, publicKey })
+  await fs.writeFile('./users.json', JSON.stringify({ users }), 'utf8')
+}
 
 const findUserFromPublicKey = async (publicKey) => {
   try {
-    const usersString = fs.readFileSync("./users.json");
-    const users = JSON.parse(usersString);
+    const usersString = fs.readFileSync('./users.json')
+    const users = JSON.parse(usersString)
 
-    const targetUser = users.find((user) => user.publicKey === publicKey);
+    const targetUser = users.find((user) => user.publicKey === publicKey)
     if (targetUser) {
-      return targetUser;
+      return targetUser
     } else {
-      throw new Error("User not found");
+      throw new Error('User not found')
     }
   } catch (error) {
-    console.error("Error: ", error);
+    console.error('Error: ', error)
   }
-};
+}
 
 const findPublicKeyFromUuid = async (uuid) => {
   try {
-    const usersString = await fs.readFile("./users.json", "utf8");
-    const users = JSON.parse(usersString);
+    const usersString = await fs.readFile('./users.json', 'utf8')
+    const users = JSON.parse(usersString)
 
-    const targetUser = users.find((user) => user.uuid === uuid);
+    const targetUser = users.find((user) => user.uuid === uuid)
     if (targetUser) {
-      return targetUser;
+      return targetUser
     } else {
-      throw new Error("User not found");
+      throw new Error('User not found')
     }
   } catch (error) {
-    console.error("Error: ", error);
+    console.error('Error: ', error)
   }
-};
+}
 
-//Server routes
-router.post("/register", registerUser);
-router.get("/message/verify", verifyMessage);
+// Server routes
+router.post('/register', registerUser)
+router.get('/message/verify', verifyMessage)
 
-//Register user
-async function registerUser(ctx) {
+// Register user
+async function registerUser (ctx) {
   try {
-    const payload = ctx.request.body;
-    const signature = payload.signature;
-    const publicKey = payload.publicKey;
+    const payload = ctx.request.body
+    const signature = payload.signature
+    const publicKey = payload.publicKey
 
     const message = JSON.stringify({
       publicKey,
       content: payload.content,
-      timestamp: payload.timestamp,
-    });
+      timestamp: payload.timestamp
+    })
 
     if (sessionless.verifySignature(signature, message, publicKey)) {
-      const uuid = sessionless.generateUUID();
-      await saveUser(uuid, publicKey);
-      let user = {
+      const uuid = sessionless.generateUUID()
+      await saveUser(uuid, publicKey)
+      const user = {
         uuid,
-        welcomeMessage: "Welcome to this example!",
-      };
-      ctx.status = 201;
-      ctx.body = JSON.stringify(user);
+        welcomeMessage: 'Welcome to this example!'
+      }
+      ctx.status = 201
+      ctx.body = JSON.stringify(user)
     } else {
-      throw new Error("Something went wrong. Please try again later.");
+      throw new Error('Something went wrong. Please try again later.')
     }
   } catch (error) {
-    ctx.status = 400;
-    ctx.body = JSON.stringify({ Error: error.message });
+    ctx.status = 400
+    ctx.body = JSON.stringify({ Error: error.message })
   }
 }
 
-//Validate message on behalf of a signer
-async function verifyMessage(ctx) {
+// Validate message on behalf of a signer
+async function verifyMessage (ctx) {
   try {
-    const payload = ctx.request.body;
-    const signature = payload.signature;
-    const uuid = payload.uuid;
+    const payload = ctx.request.body
+    const signature = payload.signature
+    const uuid = payload.uuid
 
-    const publicKey = await findPublicKeyFromUuid(uuid);
+    const publicKey = await findPublicKeyFromUuid(uuid)
 
     const message = JSON.stringify({
       content: payload.content,
-      timestamp: payload.timestamp,
-    });
+      timestamp: payload.timestamp
+    })
 
     if (sessionless.verifySignature(signature, message, publicKey)) {
-      ctx.status = 202;
+      ctx.status = 202
       ctx.body = JSON.stringify(
-        "The message content was verified successfully"
-      );
+        'The message content was verified successfully'
+      )
     } else {
-      throw new Error("Invalid request parameters provided");
+      throw new Error('Invalid request parameters provided')
     }
   } catch (error) {
-    ctx.status = 400;
-    ctx.body = JSON.stringify({ Error: error.message });
+    ctx.status = 400
+    ctx.body = JSON.stringify({ Error: error.message })
   }
 }
 
-//Start server
-app.listen(3000);
+// Start server
+app.listen(3000)
