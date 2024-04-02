b2be1c19eb6a548dca45de1f211ba8669d635b9c
omaneh <122321976+omaneh@users.noreply.github.com>
2024-04-02 15:54:56 -0400

    Edited package.json
    Former-commit-id: 4ae933629642ef1953bd8744658f5826747485ac

--- src/javascript/example/nestjs/src/main.ts
+++ src/javascript/example/nestjs/src/main.ts
@@ -5,8 +5,8 @@ import { ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(3000); 
 } 
 
 // Start server
-bootstrap();
+bootstrap();  
