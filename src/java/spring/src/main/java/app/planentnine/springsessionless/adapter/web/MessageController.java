83e2d4e5e1b62520495918407dcf4bd1cfc8997c
Daniel-J-Mason <75676396+Daniel-J-Mason@users.noreply.github.com>
2024-04-02 23:19:50 -0700

    Replaced Spring stack with MVP implementation (#19)
    * Replace initial demo folders with initial Spring build with ReadMe detailing outline and technologies used
    
    * Convert initial build to initial Hexagonal Spring stack
    
    * Add timestamps to domain objects.
    Create database adapter and modify imports/services to accommodate
    
    * add sessionless-protocol lib to project
    
    * add Sessionless verification
    
    * update message class to clearer variable name
    
    * convert user table to account table, user reserved tablename
    
    * add sessionless to allow local build
    
    * add applicaion properties for local development and deployment through docker
    
    * add sessionless libs and spring web + posgres driver
    
    * add database configs to tie application properties to build
    
    * add Dockerfile and docker-compose.yml
    
    * add controller + web dto's
    
    * remove todo
    
    * convert Sessionless to local
    
    * add UuidTypeHandler
    
    * modify implementation of generated fields
    
    * remove swagger
    
    * Bring controllers and readme in line with each other
    
    * remove unused imports
    
    * fix typo
    
    * bump gradle to latest revision
    
    * migrate util to infrastructure layer
    place sessionless in new encryption package as part of application core
    
    * modify sessionless format to standardized naming convention
    
    * update licensing to Apache 2.0
    
    Former-commit-id: c6ec130c0228eae2e3f8bced7c9bcfdf38ab65eb

---
+++ src/java/spring/src/main/java/app/planentnine/springsessionless/adapter/web/MessageController.java
@@ -0,0 +1,36 @@
+package app.planentnine.springsessionless.adapter.web;
+
+import app.planentnine.springsessionless.adapter.web.dto.RestMessageDto;
+import app.planentnine.springsessionless.adapter.web.dto.mapper.RestMessageDtoMapper;
+import app.planentnine.springsessionless.application.port.incoming.VerifyMessageUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/message")
+public class MessageController {
+    
+    private final VerifyMessageUseCase verifyMessageUseCase;
+    private final RestMessageDtoMapper messageDtoMapper;
+
+    @Autowired
+    public MessageController(VerifyMessageUseCase verifyMessageUseCase,
+                             RestMessageDtoMapper messageDtoMapper){
+        this.verifyMessageUseCase = verifyMessageUseCase;
+        this.messageDtoMapper = messageDtoMapper;
+    }
+    
+    @PostMapping("/verify")
+    public ResponseEntity<String> verifyMessage(@RequestBody RestMessageDto messageDto){
+        boolean verified = verifyMessageUseCase.verifyMessage(messageDtoMapper.map(messageDto));
+        if (verified) {
+            return ResponseEntity.accepted().body("The message content was verified successfully");
+        } else {
+            return ResponseEntity.badRequest().body("Invalid request parameters provided");
+        }
+    }
+}
