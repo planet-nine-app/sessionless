1e40fdf5f9641e8e2e880e73b296076902a2664b
Galacticai <24789792+Galacticai@users.noreply.github.com>
2024-04-03 08:21:32 +0300

    feat: constructor with string `password` to be converter to bytes

--- src/kotlin/src/main/kotlin/com/planetnine/sessionless/util/sessionless/models/KeyAccessInfo.kt
+++ src/kotlin/src/main/kotlin/com/planetnine/sessionless/util/sessionless/models/KeyAccessInfo.kt
@@ -3,4 +3,6 @@ package com.planetnine.sessionless.util.sessionless.models
 /** Information about accessing a key in secure storage
  * @param alias Key alias (used in storage)
  * @param password password securing the key pair */
-class KeyAccessInfo(val alias: String, val password: CharArray? = null)
+class KeyAccessInfo(val alias: String, val password: CharArray = CharArray(0)) {
+    constructor(alias: String, password: String) : this(alias, password.toCharArray())
+}
