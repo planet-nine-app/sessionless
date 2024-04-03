7531cd04d1c2074e37a79c3ca67006788d23c742
Galacticai <24789792+Galacticai@users.noreply.github.com>
2024-04-03 07:41:38 +0300

    feat: IdentifiableMessage. Represents a message that was signed

---
+++ src/kotlin/src/main/kotlin/com/planetnine/sessionless/util/sessionless/models/IdentifiableMessage.kt
@@ -0,0 +1,11 @@
+package com.planetnine.sessionless.util.sessionless.models
+
+/** Represents a message that was signed
+ * @param publicKey The public key used while signing
+ * @param signature The signature of the message
+ * @param message The message that was signed */
+data class IdentifiableMessage(
+    val message: String,
+    val publicKey: String,
+    val signature: MessageSignature,
+)
