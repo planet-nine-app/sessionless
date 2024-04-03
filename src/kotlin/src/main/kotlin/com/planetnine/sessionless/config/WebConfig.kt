74bb29f4791f684bad0088c6b0016c2b10c18405
Galacticai <24789792+Galacticai@users.noreply.github.com>
2024-04-03 08:22:47 +0300

    feat: configureMessageConverters
    Former-commit-id: 39ee4ea2f30b8ec830615ca2c986f1d762e5602a

--- src/kotlin/src/main/kotlin/com/planetnine/sessionless/config/WebConfig.kt
+++ src/kotlin/src/main/kotlin/com/planetnine/sessionless/config/WebConfig.kt
@@ -2,11 +2,18 @@ package com.planetnine.sessionless.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig {
+class WebConfig : WebMvcConfigurer {
+    override fun configureMessageConverters(converters: MutableList<HttpMessageConverter<*>>) {
+        converters.add(MappingJackson2HttpMessageConverter())
+        super.configureMessageConverters(converters)
+    }
+
     @Bean
     fun corsConfigurer(): WebMvcConfigurer {
         return object : WebMvcConfigurer {
