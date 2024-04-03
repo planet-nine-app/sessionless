9468346cdb9bb25f7165985c4faebbfa4a385311
Galacticai <24789792+Galacticai@users.noreply.github.com>
2024-04-03 09:23:42 +0300

    move: spring server to .../kotlin/example/spring/
    Former-commit-id: 9cb693da887088e2a6667ce6a8d876c195c8f6f9

---
+++ src/kotlin/example/spring/build.gradle.kts
@@ -0,0 +1,41 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.2.4"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
+}
+
+group = "com.planetnine"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.session:spring-session-core:3.2.2")
+    implementation("org.bouncycastle:bcprov-jdk18on:1.77")
+    implementation("org.json:json:20240303")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
