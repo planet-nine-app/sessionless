8801032401e81b3e3beff4c0b5dcbfe5e6fb6731
Galacticai <24789792+Galacticai@users.noreply.github.com>
2024-04-06 09:28:55 +0300

    add(package): kotlinx-coroutines-core
    Former-commit-id: 4fe9d80ac04b725d2e627e6944edc5997e1c8a01

--- src/kotlin/standalone/sessionless/build.gradle.kts
+++ src/kotlin/standalone/sessionless/build.gradle.kts
@@ -15,7 +15,8 @@ java {
 dependencies {
     implementation(kotlin("script-runtime"))
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
     implementation("org.bouncycastle:bcprov-jdk18on:1.77")
     implementation("org.json:json:20240303")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 }
