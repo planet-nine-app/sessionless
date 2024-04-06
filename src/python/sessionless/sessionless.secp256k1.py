44351fe47c48aa2557aa55a941ba7571b44e15ff
omaneh <122321976+omaneh@users.noreply.github.com>
2024-04-06 14:33:34 -0400

    Added all methods within the node example

--- src/python/sessionless/sessionless.secp256k1.py
+++ src/python/sessionless/sessionless.secp256k1.py
@@ -1,35 +1,52 @@
 import secp256k1
 
 class SessionlessSecp256k1():
-    def __init__(self) -> None:
+    def __init__(self):
         pass
     
-    def generateRandomPrivateKeyHex(self, saveKeys, getKeys):
-        if callable(saveKeys) and callable(getKeys):
-            return secp256k1.PrivateKey().serialize().hex()
-        return 'Not compatiable with...'
+    def generateRandomPrivateKey(self, saveKeys, getKeys):
+        try: 
+            if callable(saveKeys) and callable(getKeys):
+                return secp256k1.PrivateKey().serialize().hex()
+        except Exception as e:
+            return ""
     
-    def generateRandomPrivateKeySerialized(self, saveKeys, getKeys):
-        if callable(saveKeys) and callable(getKeys):
-            return secp256k1.PrivateKey().serialize()
-        return 'Not compatiable with...'
+    def generatePublicKeyFromPrivateKey(self, privateKeyHex):
+        try:
+            if isinstance(privateKeyHex, str) and all(c in '0123456789abcdefABCDEF' for c in privateKey):
+                deserializedPrivateKey = bytes.fromhex(privateKeyHex)
+                privateKey = secp256k1.PrivateKey()
+                privateKey.deserialize(deserializedPrivateKey)
+                return privateKey.pubkey.serialize().hex()
+        except Exception as e:
+            raise ValueError("Value not provided in correct format. Private key expected to be in hex format.")
     
-    def generatePublicKeyFromPrivateKeyHex(self, privateKey):
-        if isinstance(privateKey, str) and all(c in '0123456789abcdefABCDEF' for c in privateKey):
-            deserializedPrivateKey = bytes.fromhex(privateKey)
-            newPrivateKey = secp256k1.PrivateKey()
-            newPrivateKey.deserialize(deserializedPrivateKey)
-            return newPrivateKey.pubkey.serialize().hex()
-        raise ValueError("Value not provided in correct format. Private key expected to be in hex format.")
+    def sign(self, message, privateKeyHex):
+        try:
+            deserializedPrivateKey = bytes.fromhex(privateKeyHex)
+            privateKey = secp256k1.PrivateKey()
+            privateKey.deserialize(deserializedPrivateKey)
+            deserializedSignature = privateKey.ecdsa_sign(message)
+            signature = privateKey.ecdsa_serialize_compact(deserializedSignature)
+            return signature.hex()
+        except Exception as e:
+            pass
     
-    def generatePublicKeyFromPrivateKeySerialized(self, privateKey):
-        newPrivateKey = secp256k1.PrivateKey()
-        newPrivateKey.deserialize(privateKey)
-        return newPrivateKey.pubkey.serialize().hex()
+    def verifySignature(self, signature, message, publicKeyHex):
+        try:
+            sig = bytes.fromhex(signature)
+            deserializedPublicKey = bytes.fromhex(publicKeyHex)
+            publicKey = secp256k1.PublicKey()
+            publicKey.deserialize(deserializedPublicKey)
+            signature = publicKey.ecdsa_deserialize_compact(sig)
+            return publicKey.ecdsa_verify(message, signature)
+        except Exception as e:
+            return e
     
-    def verifySignature(self, signature, message, publicKey):
-        pass
-      
-print(secp256k1.PrivateKey().pubkey.serialize().hex())
-
+    def associate(self ):
+        try:
+            pass
+        except Exception as e:
+            pass
+    
     
