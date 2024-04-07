a6c145998207996e9f6d0ae6c299d48a0b766041
Galacticai <24789792+Galacticai@users.noreply.github.com>
2024-04-07 12:03:42 +0300

    feat: easier instantiation/conversion

--- src/csharp/Sessionless/Impl/MessageSignature.cs
+++ src/csharp/Sessionless/Impl/MessageSignature.cs
@@ -16,15 +16,23 @@ public record MessageSignatureInt : IMessageSignature {
     public MessageSignatureInt(BigInteger r, BigInteger s) {
         R = r; S = s;
     }
-
     /// <exception cref="IndexOutOfRangeException"/>
     public MessageSignatureInt(BigInteger[] ints)
             : this(ints[0], ints[1]) { }
 
+    public MessageSignatureInt(MessageSignatureHex signatureHex)
+            : this(
+                new(signatureHex.RHex, 16),
+                new(signatureHex.SHex, 16)
+            ) { }
+
     /// <summary> Convert to <see cref="MessageSignatureHex"/> </summary>
-    public MessageSignatureHex ToHex() {
-        return new(R.ToString(16), S.ToString(16));
-    }
+    public MessageSignatureHex ToHex() => new(this);
+
+
+    /// <summary> Explicit caster to <see cref="MessageSignatureHex"/> </summary> 
+    public static explicit operator MessageSignatureHex(MessageSignatureInt signatureInt)
+        => signatureInt.ToHex();
 }
 
 /// <summary> Message signature as hex <see cref="string"/>s </summary>
@@ -32,7 +40,7 @@ public record MessageSignatureHex : IMessageSignature {
     public string RHex { get; }
     public string SHex { get; }
 
-    /// <exception cref="FormatException"/>
+    /// <exception cref="HexFormatRequiredException"/>
     public MessageSignatureHex(string rHex, string sHex) {
         if (!rHex.IsHex() || !sHex.IsHex()) {
             throw new HexFormatRequiredException($"{nameof(rHex)}, {nameof(sHex)}");
@@ -40,8 +48,16 @@ public record MessageSignatureHex : IMessageSignature {
         RHex = rHex; SHex = sHex;
     }
 
+    public MessageSignatureHex(MessageSignatureInt signatureInt)
+            : this(
+                signatureInt.S.ToString(16),
+                signatureInt.S.ToString(16)
+            ) { }
+
     /// <summary> Convert to <see cref="MessageSignatureInt"/> </summary>
-    public MessageSignatureInt ToInt() {
-        return new(new(RHex, 16), new(SHex, 16));
-    }
+    public MessageSignatureInt ToInt() => new(this);
+
+    /// <summary> Explicit caster to <see cref="MessageSignatureInt"/> </summary> 
+    public static explicit operator MessageSignatureInt(MessageSignatureHex signatureHex)
+        => signatureHex.ToInt();
 }
