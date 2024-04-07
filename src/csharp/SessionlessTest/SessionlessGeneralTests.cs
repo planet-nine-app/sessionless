9c42013a92e1cfc91ad56e2f5acb3e83a46170be
Galacticai <24789792+Galacticai@users.noreply.github.com>
2024-04-07 12:21:35 +0300

    update: tests setup
    Former-commit-id: dcc3cb7496bfedc4a8aabaceb50a3e071be56983

--- src/csharp/SessionlessTest/SessionlessGeneralTests.cs
+++ src/csharp/SessionlessTest/SessionlessGeneralTests.cs
@@ -8,23 +8,24 @@ public class SessionlessGeneralTests {
     Vault V;
     Sessionless S;
 
-    [SetUp]
-    public void Setup() {
-        string privPath = "./priv.key";
-        string pubPath = "./pub.key";
-        V = new Vault(
-            getter: () => {
+    const string privPath = "./priv.key";
+    const string pubPath = "./pub.key";
+    static void VaultSaver(KeyPairHex pair) {
+        File.WriteAllText(privPath, pair.PrivateKey);
+        File.WriteAllText(pubPath, pair.PublicKey);
+    }
+    static KeyPairHex? VaultGetter() {
                 if (!File.Exists(privPath)) return null;
                 if (!File.Exists(pubPath)) return null;
                 var privateHex = File.ReadAllText(privPath);
                 var publicHex = File.ReadAllText(pubPath);
                 return new(privateHex, publicHex);
-            },
-            saver: (KeyPairHex pair) => {
-                File.WriteAllText(privPath, pair.PrivateKey);
-                File.WriteAllText(pubPath, pair.PublicKey);
             }
-        );
+
+
+    [SetUp]
+    public void Setup() {
+        V = new Vault(VaultGetter, VaultSaver);
         S = new(V);
     }
 
