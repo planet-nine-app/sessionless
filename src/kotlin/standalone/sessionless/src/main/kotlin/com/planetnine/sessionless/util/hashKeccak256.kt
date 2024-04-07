66f769bd8d0eeaf3c88802713b301f3ec24e15a7
Galacticai <24789792+Galacticai@users.noreply.github.com>
2024-04-07 13:13:56 +0300

    update: explicit return type

--- src/kotlin/standalone/sessionless/src/main/kotlin/com/planetnine/sessionless/util/hashKeccak256.kt
+++ src/kotlin/standalone/sessionless/src/main/kotlin/com/planetnine/sessionless/util/hashKeccak256.kt
@@ -2,4 +2,5 @@ package com.planetnine.sessionless.util
 
 import org.bouncycastle.jcajce.provider.digest.Keccak
 
-fun hashKeccak256(message: String) = Keccak.Digest256().digest(message.toByteArray())
+fun hashKeccak256(message: String): ByteArray =
+    Keccak.Digest256().digest(message.toByteArray())
