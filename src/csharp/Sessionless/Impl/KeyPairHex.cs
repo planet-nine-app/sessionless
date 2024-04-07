278dcdd4c7dfe1f4b937556962a19efea2ffc9ab
Galacticai <24789792+Galacticai@users.noreply.github.com>
2024-04-07 12:15:17 +0300

    fix: throw if not hex
    Former-commit-id: 06e47d85cf516086dc12b622c4a2e7b76e7de7a6

--- src/csharp/Sessionless/Impl/KeyPairHex.cs
+++ src/csharp/Sessionless/Impl/KeyPairHex.cs
@@ -1,9 +1,17 @@
-﻿namespace SessionlessNET.Impl;
+﻿using SessionlessNET.Impl.Exceptions;
+using SessionlessNET.Util;
+
+namespace SessionlessNET.Impl;
 
 public record KeyPairHex {
     public string PrivateKey { get; }
     public string PublicKey { get; }
+
+    /// <exception cref="HexFormatRequiredException"/>
     public KeyPairHex(string privateKey, string publicKey) {
+        if (!privateKey.IsHex() || !publicKey.IsHex()) {
+            throw new HexFormatRequiredException($"{nameof(privateKey)}, {nameof(publicKey)}");
+        }
         PrivateKey = privateKey;
         PublicKey = publicKey;
     }
