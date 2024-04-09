175b628acb2fff79f872a5371c03689a67a409ce
omaneh <122321976+omaneh@users.noreply.github.com>
2024-04-09 11:49:45 -0400

    Began adding test cases
    Former-commit-id: 10df6df99c7055ce1742764c99cf40a253c42c8e

--- src/python/package/sessionless/modelsecp256k1.py
+++ src/python/package/sessionless/modelsecp256k1.py
@@ -4,7 +4,7 @@ class SessionlessSecp256k1():
     def __init__(self, private_key_hex=secp256k1.PrivateKey().serialize()):
         private_key = secp256k1.PrivateKey()
         assert private_key.deserialize(private_key_hex) == private_key.private_key
-        self.private_key = private_key
+        self.__private_key = private_key
     
     def get_algorithm_name(self):
         return "secp256k1"
@@ -12,7 +12,7 @@ class SessionlessSecp256k1():
     def get_private_key(self, saveKeys, getKeys):
         try: 
             if callable(saveKeys) and callable(getKeys):
-                return self.private_key.serialize()
+                return self.__private_key.serialize()
         except Exception as e:
             raise TypeError("No default secure storage in python. Please provide a saveKeys and getKeys function to store private key. Internal error message: " + e)
         
@@ -20,14 +20,14 @@ class SessionlessSecp256k1():
         try:
             private_key = secp256k1.PrivateKey()
             assert private_key.deserialize(new_private_key_hex) == private_key.private_key
-            self.private_key = private_key
+            self.__private_key = private_key
             return "Updated private key value."
         except Exception as e:
             raise AttributeError("Private key unable to be assigned. Please provide a valid key in hex format and try again. Internal error message: " + e)
     
     def get_public_key_from_private_key(self):
         try:
-            return self.private_key.pubkey.serialize().hex()
+            return self.__private_key.pubkey.serialize().hex()
         except Exception as e:
             raise ValueError("Value not provided in correct format. Private key expected to be in hex format. Internal error message: " + e)
     
@@ -35,22 +35,19 @@ class SessionlessSecp256k1():
         try:
             if not isinstance(msg, bytes):
                 msg = pickle.dumps(msg)
-            deserializedSig = self.private_key.ecdsa_sign(msg)
-            sig = self.private_key.ecdsa_serialize_compact(deserializedSig)
+            deserializedSig = self.__private_key.ecdsa_sign(msg)
+            sig = self.__private_key.ecdsa_serialize_compact(deserializedSig)
             return sig.hex()
         except Exception as e:
             raise ValueError("Value not provided in correct format. Internal error message: " + e)
         
-    def verify_signature(self, signature, msg, public_key_hex):
+    def verify_signature(self, signature, msg):
         try:
             if not isinstance(msg, bytes):
                 msg = pickle.dumps(msg)
             sig = bytes.fromhex(signature)
-            deserializedPublicKey = bytes.fromhex(public_key_hex)
-            publicKey = secp256k1.PublicKey()
-            publicKey.deserialize(deserializedPublicKey)
-            signature = publicKey.ecdsa_deserialize_compact(sig)
-            return publicKey.ecdsa_verify(msg, signature)
+            signature = self.__private_key.pubkey.ecdsa_deserialize_compact(sig)
+            return self.__private_key.pubkey.ecdsa_verify(msg, signature)
         except Exception as e:
             return ValueError("Error with parameters. Please ensure values are provided in correct format. Internal error message: " + e)
     
@@ -59,4 +56,3 @@ class SessionlessSecp256k1():
             return (self.verifySignature(primarySignature, primaryMessage, primaryPublicKey) and self.verifySignature(secondarySignature, secondaryMessage, secondaryPublicKey))
         except Exception as e:
             pass
-    
--- src/python/package/tests/test.modelsecp256k1.py
+++ src/python/package/tests/test.modelsecp256k1.py
@@ -1,19 +1,70 @@
 import unittest
 from sessionless import SessionlessSecp256k1
 
+PRIVATE_KEY_1 = "8dea0f7f726273b6971c1a4bcc3d60d8006fc087a4b9c4e7ae633918dff54f26"
+PRIVATE_KEY_2 = "68eab2d2fae06bbd24d633ecb72d9a8d6c9e2de0aef7499028d471b8ad4cb204"
+
+PUBLIC_KEY_1 = "036c2d503304b96b59bfb5f40e7d8b7f84646c21923a2eb19108b84afbd808f317"
+PUBLIC_KEY_2 = "03a68885996743ac828dcb3d7747cb1758bca819409f86dfd017acac1eda98fd69"
+
+MSG = "Test"
+
+SIG_1 = "34cd9384267027bbaedff2727252a52d77aee14f266594584a18d3d69e3f0b8b1a1e597fe428829e4ef850cfd6c032212e6e2eb35827056d8fd403a5a571939a"
+SIG_2 = "db1ac1ad92ad24c0cfd89f055250c5e6c187d859c134016fed1d7c631260823146d01c3f5a082fde987d413c58f8c04faa5f6da00b99d003d1d3a691deedba6b"
+
+def getKey():
+        pass
+
+def saveKey():
+        pass
 class SessionlessSecp256k1Test(unittest.TestCase):
+   
         def test_private_key_hex(self):
-                pass
+                sl = SessionlessSecp256k1(PRIVATE_KEY_1)
+                result = sl.get_private_key(saveKey, getKey)
+                self.assertEqual(result, PRIVATE_KEY_1)
+                
+                sl2 = SessionlessSecp256k1(PRIVATE_KEY_2)
+                result2 = sl2.get_private_key(saveKey, getKey)
+                self.assertEqual(result2, PRIVATE_KEY_2)
         
         def test_public_key_from_hex(self):
-                pass
+                sl = SessionlessSecp256k1(PRIVATE_KEY_1)
+                result = sl.get_public_key_from_private_key()
+                self.assertEqual(result, PUBLIC_KEY_1)
+                
+                sl2 = SessionlessSecp256k1(PRIVATE_KEY_2)
+                result2 = sl2.get_public_key_from_private_key()
+                self.assertEqual(result2, PUBLIC_KEY_2)
+                
         
         def test_signing_message(self):
-                pass
+                sl = SessionlessSecp256k1(PRIVATE_KEY_1)
+                result = sl.sign_message(MSG)
+                self.assertEqual(result, SIG_1)
+                
+                sl2 = SessionlessSecp256k1(PRIVATE_KEY_2)
+                result2 = sl2.sign_message(MSG)
+                self.assertEqual(result2, SIG_2)
         
         def test_verifying_signature(self):
-                pass
+                sl = SessionlessSecp256k1(PRIVATE_KEY_1)
+                sig = sl.sign_message(MSG)
+                
+                sl2 = SessionlessSecp256k1(PRIVATE_KEY_2)
+                sig2 = sl2.sign_message(MSG)
+                
+                res1 = sl.verify_signature(sig2, MSG) 
+                res2 = sl.verify_signature(sig, MSG)
+                res3 = sl2.verify_signature(sig2, MSG) 
+                res4 = sl2.verify_signature(sig, MSG)
+                
+                self.assertEqual(res1, False)
+                self.assertEqual(res2, True)
+                self.assertEqual(res3, True)
+                self.assertEqual(res4, False)
+        
         
         def test_associating_verification(self):
                 pass
-                
+
