c973f629f367b62f95e3e41c5e2157618453ba16
FssAy <49795742+FssAy@users.noreply.github.com>
2024-04-10 00:20:27 +0000

    Update Rust implementation (#27)

--- src/rust/examples/web-server/src/handler/resources.rs
+++ src/rust/examples/web-server/src/handler/resources.rs
@@ -1,23 +1,33 @@
+use super::*;
 use hyper::header::CONTENT_TYPE;
 use hyper::http::HeaderValue;
-use super::*;
 
 macro_rules! include_resource {
-    ($builder:ident, $path:literal, $mime:literal) => {
-        {
-            $builder.set_header(CONTENT_TYPE, HeaderValue::from_static($mime));
-            include_bytes!($path).as_slice()
-        }
-    };
+    ($builder:ident, $path:literal, $mime:literal) => {{
+        $builder.set_header(CONTENT_TYPE, HeaderValue::from_static($mime));
+        include_bytes!($path).as_slice()
+    }};
 }
 
-pub async fn load(head: &Parts, _body: Incoming, builder: &mut response::Builder) -> anyhow::Result<()> {
+pub async fn load(
+    head: &Parts,
+    _body: Incoming,
+    builder: &mut response::Builder,
+) -> anyhow::Result<()> {
     let path = head.uri.path();
 
     let data = match path {
         "/client" => include_resource!(builder, "../../frontend/pages/client.html", "text/html"),
-        "/web_client.js" => include_resource!(builder, "../../frontend/libs/web_client.js", "text/javascript"),
-        "/web_client_bg.wasm" => include_resource!(builder, "../../frontend/libs/web_client_bg.wasm", "application/wasm"),
+        "/web_client.js" => include_resource!(
+            builder,
+            "../../frontend/libs/web_client.js",
+            "text/javascript"
+        ),
+        "/web_client_bg.wasm" => include_resource!(
+            builder,
+            "../../frontend/libs/web_client_bg.wasm",
+            "application/wasm"
+        ),
         _ => return Err(anyhow!("Resource not found!")),
     };
 
