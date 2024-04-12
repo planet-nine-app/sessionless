5ab635479722e264d6130ff00f7047d88434fbfa
omaneh <122321976+omaneh@users.noreply.github.com>
2024-04-12 18:53:22 -0400

    Modified methods and tests. All tests ran
    Former-commit-id: debc4448dec6af748f60b1b35ad37ea74ff7ce4c

--- src/python/package/sessionless/modelsecp256k1.py
+++ src/python/package/sessionless/modelsecp256k1.py
@@ -1,58 +1,58 @@
 import secp256k1
 import pickle
 import uuid
+import inspect
+
+
 class SessionlessSecp256k1():
     
-    def __init__(self, private_key_hex=None):
-        if private_key_hex is not None:
-            private_key_hex = secp256k1.PrivateKey().serialize()
-        private_key = secp256k1.PrivateKey()
-        assert private_key.deserialize(private_key_hex) == private_key.private_key
-        self.__private_key = private_key
-    
+    def __init__(self, get_keys):
+        self.get_keys = get_keys
+        
     def generateUUID(self):
         return uuid.uuid4().hex
     
     def generate_keys(self, saveKeys, getKeys):
         try: 
             if callable(saveKeys) and callable(getKeys):
-                private_key = self.__private_key.serialize()
-                public_key = self.__private_key.pubkey.serialize().hex()
+                private_key_obj = secp256k1.PrivateKey()
+                private_key = private_key_obj.serialize()
+                public_key = private_key_obj.pubkey.serialize().hex()
                 return private_key, public_key
-        except Exception as e:
-            raise TypeError("No default secure storage in python. Please provide a saveKeys and getKeys function to store private key. Internal error message: " + e)
+        except Exception:
+            raise TypeError("No default secure storage in python. Please provide a saveKeys and getKeys function to store private key. Internal error message: ")
     
-    def sign(self, msg):
+    async def sign(self, msg):
         try:
             if not isinstance(msg, bytes):
                 msg = pickle.dumps(msg)
-            deserializedSig = self.__private_key.ecdsa_sign(msg)
-            sig = self.__private_key.ecdsa_serialize_compact(deserializedSig)
+            if inspect.iscoroutinefunction(self.get_keys):
+                private_key_hex = await self.get_keys()
+            else: 
+                private_key_hex = await self.get_keys()
+            private_key = secp256k1.PrivateKey()
+            assert private_key.deserialize(private_key_hex) == private_key.private_key
+            deserializedSig = private_key.ecdsa_sign(msg)
+            sig = private_key.ecdsa_serialize_compact(deserializedSig)
             return sig.hex()
-        except Exception as e:
-            raise ValueError("Value not provided in correct format. Internal error message: " + e)
+        except Exception:
+            raise ValueError("Value not provided in correct format. Internal error message: ")
         
-    def verify_signature(self, signature, msg, public_key_hex=None):
+    def verify_signature(self, signature, msg, public_key_hex):
         try:
             if not isinstance(msg, bytes):
                 msg = pickle.dumps(msg)
             sig = bytes.fromhex(signature)
-            if public_key_hex is not None:
-                public_key = secp256k1.PublicKey()
-                public_key.deserialize(bytes.fromhex(public_key_hex))
-            else:
-                public_key = self.__private_key.pubkey
+            public_key = secp256k1.PublicKey()
+            assert public_key.deserialize(bytes.fromhex(public_key_hex)) == public_key.public_key
+            
             signature = public_key.ecdsa_deserialize_compact(sig)
             return public_key.ecdsa_verify(msg, signature)
-        except Exception as e:
-            raise ValueError("Error with parameters. Please ensure values are provided in correct format. Internal error message: " + e)
+        except Exception:
+            raise ValueError("Error with parameters. Please ensure values are provided in correct format. Internal error message: ")
     
     def associate(self, primary_sig, primary_msg, primary_public_key, secondary_sig, secondary_msg, secondary_public_key ):
         try:
-            return (self.verifySignature(primary_sig, primary_msg, primary_public_key) and self.verifySignature(secondary_sig, secondary_msg, secondary_public_key))
-        except Exception as e:
-            raise ValueError("Error with parameters. Please ensure values are provided in correct format. Internal error message: " + e)
-
-
-
-
+            return (self.verify_signature(primary_sig, primary_msg, primary_public_key) and self.verify_signature(secondary_sig, secondary_msg, secondary_public_key))
+        except Exception:
+            raise ValueError("Error with parameters. Please ensure values are provided in correct format. Internal error message: ")
