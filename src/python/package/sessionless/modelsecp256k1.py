8867c9c2db549a1e2fe3e6c85df4037bcaaa206c
Habso <122321976+habomane@users.noreply.github.com>
2024-04-13 12:45:42 -0400

    Python (#28)
    * Adding initial files
    
    * Altered structure of the package. Renamed core files
    
    * Altered structure of package.
    
    * Delete src/python/.venv directory
    
    * Added functions to the secp256k1 file
    
    * Added all methods within the node example
    
    * Edited methods to reduce repititive code.
    
    * Updated file name to prevent circular importing due to same name of file and package
    
    * Pushed things that should not have been pushed
    
    * Trying again
    
    * Updating gitignore
    
    * Delete src/python/sessionless/__pycache__ directory
    
    * Added some methods to test case
    
    * Added test case definitions
    
    * Changed file structure
    
    * Update README.md
    
    * Update README.md
    
    * Began adding test cases
    
    * Updated the verify signature and associate method
    
    * Added last test case
    
    * Updated README
    
    * Small edits
    
    * Modified methods to comply with protocol
    
    * Modified methods and tests. All tests ran
    
    * Update README.md
    
    * Small tweaks to naming
    
    * Update README.md
    
    grammarly
    
    * Updating tests
    
    * Update modelsecp256k1.py
    
    * Update modelsecp256k1.py
    
    * Update modelsecp256k1.py
    
    ---------
    
    Co-authored-by: omaneh <122321976+omaneh@users.noreply.github.com>

---
+++ src/python/package/sessionless/modelsecp256k1.py
@@ -0,0 +1,58 @@
+import secp256k1
+import pickle
+import uuid
+import inspect
+
+
+class SessionlessSecp256k1():
+    
+    def __init__(self, get_keys):
+        self.get_keys = get_keys
+        
+    def generate_UUID(self):
+        return uuid.uuid4().hex
+    
+    def generate_keys(self, save_key):
+        try: 
+            if callable(save_key):
+                private_key_obj = secp256k1.PrivateKey()
+                private_key = private_key_obj.serialize()
+                public_key = private_key_obj.pubkey.serialize().hex()
+                return private_key, public_key
+        except Exception:
+            raise TypeError("No default secure storage in python. Please provide a callable method to store private key.")
+    
+    async def sign(self, msg):
+        try:
+            if not isinstance(msg, bytes):
+                msg = pickle.dumps(msg)
+            if inspect.iscoroutinefunction(self.get_keys):
+                private_key_hex = await self.get_keys()
+            else: 
+                private_key_hex = self.get_keys()
+            private_key = secp256k1.PrivateKey()
+            assert private_key.deserialize(private_key_hex) == private_key.private_key
+            deserialized_sig = private_key.ecdsa_sign(msg)
+            sig = private_key.ecdsa_serialize_compact(deserialized_sig)
+            return sig.hex()
+        except Exception:
+            raise ValueError("Error with parameters. Please ensure values are provided in correct format.")
+        
+    def verify_signature(self, signature, msg, public_key_hex):
+        try:
+            if not isinstance(msg, bytes):
+                msg = pickle.dumps(msg)
+            sig = bytes.fromhex(signature)
+            public_key = secp256k1.PublicKey()
+            assert public_key.deserialize(bytes.fromhex(public_key_hex)) == public_key.public_key
+            
+            signature = public_key.ecdsa_deserialize_compact(sig)
+            return public_key.ecdsa_verify(msg, signature)
+        except Exception:
+            raise ValueError("Error with parameters. Please ensure values are provided in correct format.")
+    
+    def associate(self, primary_sig, primary_msg, primary_public_key, secondary_sig, secondary_msg, secondary_public_key ):
+        try:
+            return (self.verify_signature(primary_sig, primary_msg, primary_public_key) and self.verify_signature(secondary_sig, secondary_msg, secondary_public_key))
+        except Exception:
+            raise ValueError("Error with parameters. Please ensure values are provided in correct format.")
