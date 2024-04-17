ed26229a0ff16359481522140bed6c05e255e2bc
zach-planet-nine <zach@planetnine.app>
2024-04-16 23:45:20 -0700

    lots of tests
    Former-commit-id: 3a940023dcd8eeefb98adfea5de30d5248439558

--- src/javascript/example/cli/main.js
+++ src/javascript/example/cli/main.js
@@ -46,5 +46,8 @@ program.parse();
 
 const options = program.opts();
 const limit = options.first ? 1 : undefined;
+console.log(options);
+const limit = options.first ? 1 : undefined;
+console.log(program.args[0].split(options.separator, limit));
 
 
