72deb0cef3a8ba654fd7112475a0acbde9cf6ed6
Daniel-J-Mason <daniel.j.msn@gmail.com>
2024-04-17 21:59:48 -0700

    remove unused imports

--- src/java/spring/src/main/java/app/planentnine/springsessionless/adapter/web/MessageController.java
+++ src/java/spring/src/main/java/app/planentnine/springsessionless/adapter/web/MessageController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MessageController {
--- src/java/spring/src/main/java/app/planentnine/springsessionless/adapter/web/UserController.java
+++ src/java/spring/src/main/java/app/planentnine/springsessionless/adapter/web/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.math.BigInteger;
 import java.util.HashMap;
