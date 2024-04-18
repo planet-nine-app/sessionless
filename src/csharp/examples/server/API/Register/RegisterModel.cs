5c8f69fdf079d5c8bf66498d92f03723ace19060
zach-planet-nine <zach@planetnine.app>
2024-04-17 23:36:37 -0700

    c sharp server started
    Former-commit-id: 3a7900788c06e3dcb21ab881dba1700c5c8adaec

---
+++ src/csharp/examples/server/API/Register/RegisterModel.cs
@@ -0,0 +1,10 @@
+namespace server.Models
+{
+    public class RegisterModel
+    {
+        public string pubKey { get; } = "default";
+        public string enteredText { get; } = "default";
+        public string timestamp { get; } = "default";
+
+    }
+}
