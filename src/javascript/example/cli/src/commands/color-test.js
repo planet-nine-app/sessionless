4fe12998f3ed62fe1f91d6b03c34c71f49a56ba7
zach-planet-nine <zach@planetnine.app>
2024-04-21 14:14:10 -0700

    Fixed js bug, and added lots of users test
    Former-commit-id: 68d102851df85151bfdf7b9681a91971804ed51d

--- src/javascript/example/cli/src/commands/color-test.js
+++ src/javascript/example/cli/src/commands/color-test.js
@@ -4,6 +4,7 @@ import doCoolStuff from '../requests/do-cool-stuff.js';
 
 const colorTest = async (color, language) => {
 <<<<<<< HEAD
+<<<<<<< HEAD
   await register(color)
   .then((res) => doCoolStuff(res.body))
   .then((res) => {
@@ -15,6 +16,14 @@ const colorTest = async (color, language) => {
   .then((res) => doCoolStuff(res.body))
   .then((res) => {
 >>>>>>> e2378953 (tests and csharp server example)
+=======
+  await register(color)
+  .then((res) => doCoolStuff(res.body))
+  .then((res) => {
+    if(!res.doubleCool) {
+      throw(chalk.red('OH NO YOUR SHIT BLEW UP AGAIN!!!!'));
+    }
+>>>>>>> 1d7f9ef6 (Fixed js bug, and added lots of users test)
     console.log(chalk[color](`Aww yeah! The ${language ? language : ''} server thinks you're ${res.doubleCool}.`));
   })
   .catch(console.error);
