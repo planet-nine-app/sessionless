680e1d7dd5b7a936e5ddd4144de5cf6637e006b0
Galacticai <24789792+Galacticai@users.noreply.github.com>
2024-04-26 20:51:06 +0300

    add: some docs for `SignedMessage` and its subclasses

--- src/csharp/Sessionless/Impl/SignedMessage.cs
+++ src/csharp/Sessionless/Impl/SignedMessage.cs
@@ -5,24 +5,32 @@ using SessionlessNET.Util;
 
 namespace SessionlessNET.Models;
 
+/// <summary> A <see cref="Message"/> that has been signed, having the given <see cref="Signature"/> </summary>
 public record SignedMessage {
+    /// <summary> Original <see cref="string"/> that was signed </summary>
     public string Message { get; }
+    /// <summary> <see cref="Message"/> signature </summary>
     public MessageSignatureHex Signature { get; }
 
     public SignedMessage(string message, MessageSignatureHex signature) {
         Message = message;
         Signature = signature;
     }
+
+    /// <summary> Add the public key (<see cref="string"/>) that was used to sign the message </summary>
+    /// <returns> this but as <see cref="SignedMessageWithKey"/> </returns>
     public SignedMessageWithKey WithKey(string publicKey)
         => new(this, publicKey);
 
+    /// <summary> Add the public key (<see cref="ECPublicKeyParameters"/>) that was used to sign the message </summary>
+    /// <returns> this but as <see cref="SignedMessageWithECKey"/> </returns>
     public SignedMessageWithECKey WithKey(ECPublicKeyParameters publicKey)
         => new(this, publicKey);
 }
 
 /// <summary> <see cref="SignedMessage"/> but with a public key <see cref="string"/> </summary>
 public record SignedMessageWithKey : SignedMessage {
-    /// <summary> Public key as a <see cref="string"/> of bytes </summary>
+    /// <summary> Public key that was used to sign the <see cref="SignedMessage.Message"/> (as a <see cref="string"/> of bytes) </summary>
     public string PublicKey { get; }
 
     public SignedMessageWithKey(string message, MessageSignatureHex signature, string publicKey)
@@ -36,13 +44,13 @@ public record SignedMessageWithKey : SignedMessage {
             : this(signedMessage.Message, signedMessage.Signature, publicKey) { }
 
     /// <summary> Convert to <see cref="SignedMessageWithECKey"/> </summary>
-
     public SignedMessageWithECKey ToEC()
         => new(this, PublicKey.ToEC());
 }
 
 /// <summary> <see cref="SignedMessage"/> but with a public key of type <see cref="ECPublicKeyParameters"/> </summary>
 public record SignedMessageWithECKey : SignedMessage {
+    /// <summary> Public key that was used to sign the <see cref="SignedMessage.Message"/> (as <see cref="ECPublicKeyParameters"/>) </summary>
     public ECPublicKeyParameters PublicKey { get; }
 
     public SignedMessageWithECKey(string message, MessageSignatureHex signature, ECPublicKeyParameters publicKey)
