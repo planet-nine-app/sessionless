9a78b1c57dd814fd007f5e9f06bf17667fc5ce65
FssAy <49795742+FssAy@users.noreply.github.com>
2024-05-03 15:02:18 +0200

    Remove async comments
    Former-commit-id: d4cc918ade45ac55f8d64355522a7c34e1af2bd7

--- src/rust/examples/slapper/src/commands/color_test.rs
+++ src/rust/examples/slapper/src/commands/color_test.rs
@@ -1,6 +1,6 @@
 use crate::requests;
 
-/*async*/ pub fn color_test(color: Option<String>, _language: Option<String>, _iterations: Option<u32>) {
+pub fn color_test(color: Option<String>, _language: Option<String>, _iterations: Option<u32>) {
     let unwrapped_color = color.unwrap_or("no color".to_string());
     
     if unwrapped_color == "no color" {
