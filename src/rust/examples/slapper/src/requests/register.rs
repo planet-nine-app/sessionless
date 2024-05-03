2e124448e3bbf480f4b8b88c04b5ce823d312e8f
FssAy <49795742+FssAy@users.noreply.github.com>
2024-05-03 16:00:01 +0200

    Improve `register` request

--- src/rust/examples/slapper/src/requests/register.rs
+++ src/rust/examples/slapper/src/requests/register.rs
@@ -1,47 +1,73 @@
 use sessionless::hex::IntoHex;
-use sessionless::Sessionless;
+use sessionless::{Sessionless, Signature};
 use reqwest;
+use serde::Serialize;
 use crate::requests::WelcomeResponse;
 use crate::utils::Color;
 
+#[derive(Debug, Serialize)]
+struct Message<'a> {
+    pub_key: String,
+    entered_text: &'a str,
+    timestamp: &'a str,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    signature: Option<String>,
+}
+
+impl<'a> Message<'a> {
+    fn as_json(&self) -> String {
+        serde_json::to_string(&self).unwrap()
+    }
+
+    fn sign(&self, sessionless: &Sessionless) -> Signature {
+        let message_json = self.as_json();
+        println!("Signing {}", message_json);
+        sessionless.sign(message_json.as_bytes())
+    }
+}
+
 pub fn register(color: Color) -> (Sessionless, WelcomeResponse) {
     let sessionless = Sessionless::new();
+    let client = reqwest::blocking::Client::new();
 
-    let base_url = color.get_url();
     let placement = color.get_signature_placement();
 
-    let public_key = sessionless.public_key().to_hex();
-    let entered_text = "Foo";
-    let timestamp = "right now";
+    let mut message = Message {
+        pub_key: sessionless.public_key().to_hex(),
+        entered_text: "Foo",
+        timestamp: "right now",
+        signature: None,
+    };
 
-    let message = format!(r#"{{"pubKey":"{0}","enteredText":"{1}","timestamp":"{2}"}}"#, public_key, entered_text, timestamp);   
-    println!("Signing {0}", message); 
-    let signature = sessionless.sign(message.as_bytes()).into_hex();
+    let signature = message.sign(&sessionless).to_hex();
 
-    let client = reqwest::blocking::Client::new();
-    let url = format!("{}/register", base_url);
-    println!("{}", url);
-    let mut post = client.post(url)
+    let mut request_builder = client
+        .post({
+            let url = format!("{}/register", color.get_url());
+            println!("{}", url);
+            url
+        })
         .header("Content-Type", "application/json")
         .header("Accept", "application/json");
 
     if placement == "payload" {
-        let payload = format!(r#"{{"pubKey":"{0}","enteredText":"{1}","timestamp":"{2}","signature":"{3}"}}"#, public_key, entered_text, timestamp, signature);
-        post = post.body(payload)
+        message.signature = Some(signature);
+        request_builder = request_builder
+            .body(message.as_json());
     } else {
-        post = post.header("signature", signature)
-        .body(message);
+        request_builder = request_builder
+            .header("signature", signature)
+            .body(message.as_json());
     }
-    let result = post.send(); 
-    let response = match result {
-        Ok(resp) => resp,
-        Err(_) => panic!("Something went awry!")
-    };
-    let json_result = response.json::<WelcomeResponse>();
-    let welcome_response: WelcomeResponse = match json_result {
+
+    let welcome_response = match request_builder
+        .send()
+        .expect("Something went awry!")
+        .json::<WelcomeResponse>()
+    {
         Ok(wr) => wr,
         Err(_) => panic!("Error serializing JSON")
     };
-//    let welcome_response: WelcomeResponse = result.expect("response should be json").json::<WelcomeResponse>();
+
     return (sessionless, welcome_response);
 }
