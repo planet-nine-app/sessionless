ad5032d11efd0fd16258ffa5c10ca5bb7b951601
FssAy <49795742+FssAy@users.noreply.github.com>
2024-05-03 17:32:57 +0200

    Rework `requests` module
    Former-commit-id: 7adb7831aa1a7b2a0099a5d0e3433bdcde022f1c

--- src/rust/examples/slapper/src/commands/color_test.rs
+++ src/rust/examples/slapper/src/commands/color_test.rs
@@ -1,19 +1,16 @@
 use anyhow::anyhow;
-use crate::requests;
+use sessionless::Sessionless;
+use crate::requests::register::RegisterRequest;
+use crate::requests::*;
+use crate::requests::do_cool_stuff::CoolRequest;
 use crate::utils::Color;
 
 pub fn color_test(color: Option<Color>, _language: Option<String>, _iterations: Option<u32>) -> anyhow::Result<()> {
     let color = color.ok_or_else(|| anyhow!("Need a color for a color test!"))?;
+    let sessionless = Sessionless::new();
 
-    let register_tuple = requests::register(color)?;
-    requests::do_cool_stuff(color, register_tuple.0, register_tuple.1)?;
+    let register_response = RegisterRequest::execute(color, &sessionless, ())?;
+    CoolRequest::execute(color, &sessionless, register_response)?;
 
     Ok(())
-    /*    if cool_stuff_response.doubleCool != null {
-            let success = "Aww yeah! The {language} server thinks you're {coolStuffResponse.doubleCool}".green();
-            println!("{}", success.to_string());
-        } else {
-            let fail = "OH NO, THIS DIDN'T WORK".red();
-            println!("{}", fail.to_string());
-        }*/
 }
