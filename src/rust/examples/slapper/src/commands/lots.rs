6878e2045386875d7b59935ad304fbf79021bf83
FssAy <49795742+FssAy@users.noreply.github.com>
2024-05-03 15:04:53 +0200

    Refactor commands module
    Former-commit-id: 46dad83f03536458aaed4193ca4a753de02936be

---
+++ src/rust/examples/slapper/src/commands/lots.rs
@@ -0,0 +1,4 @@
+pub fn lots(_color: Option<String>, _language: Option<String>, _iterations: Option<u32>) {
+    // TODO: Implement this. Remove the _'s from args above when you do
+    println!("Lots");
+}
