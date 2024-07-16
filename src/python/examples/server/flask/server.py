3067de28f5090d3d6115c81bf0dc2d746f7a0576
zach-planet-nine <zach@planetnine.app>
2024-07-15 23:14:24 -0700

    python server

--- src/python/examples/server/flask/server.py
+++ src/python/examples/server/flask/server.py
@@ -1,34 +1,52 @@
-from flask import Flask
+from flask import Flask, request
 from sessionless import SessionlessSecp256k1
 from src.user.user import create_user
 from src.user.user import get_user
 app = Flask(__name__)
 
+import logging
+
 @app.errorhandler(403)
 def resource_not_found(e):
     return jsonify(error=str(e)), 403
 
-@app.route('/register', methods=('POST'))
-def create_user:
-    pubKey = request.args.get('pubKey')
-    enteredText = request.args.get('enteredText')
-    timestamp = request.args.get('timestamp')
-    signature = request.args.get('signature')
-    message = f'{{"pubKey":"{public_key}","enteredText":"Foo","timestamp":"{timestamp}"}}'
+@app.route('/register', methods=['POST'])
+def register():
+    json = request.json
+    pubKey = json['pubKey']
+    enteredText = json['enteredText']
+    timestamp = json['timestamp']
+    signature = json['signature']
+    message = f'{{"pubKey":"{pubKey}","enteredText":"Foo","timestamp":"{timestamp}"}}'
 
-    if !sessionless.verifySignature(signature, message, pubKey): 
+    sessionless = SessionlessSecp256k1()
+    verified = sessionless.verifySignature(signature, message, pubKey)
+    if not verified:
         abort(403, description="Auth error")
 
     uuid = create_user(pubKey)
     resp = { 
-        "uuid" = uuid,
-        "welcomeMessage" = "Welcome to Sessionless"
+        "uuid": uuid,
+        "welcomeMessage": "Welcome to Sessionless"
     }
     return resp
 
-@app.route('/cool-stuff')
+@app.route('/cool-stuff', methods=['POST'])
 def do_cool_stuff():
-    // get user
-    // auth
-    // return cool stuff
-    return // python dictionary of coolness = doubleCool
+    json = request.json
+    uuid = json['uuid']
+    coolness = json['coolness']
+    timestamp = json['timestamp']
+    signature = json['signature']
+    message = f'{{"uuid":"{uuid}","coolness":"max","timestamp":"{timestamp}"}}'
+    user = get_user(uuid)
+
+    sessionless = SessionlessSecp256k1()
+    verified = sessionless.verifySignature(signature, message, user['pubKey'])
+    if not verified:
+        abort(403, description="Auth error")
+
+    resp = {
+        "doubleCool": "double cool"
+    }
+    return resp
