1e08605fc5fdb08d6412a5de0d412715c2a47c87
Galacticai <24789792+Galacticai@users.noreply.github.com>
2024-07-26 02:15:08 +0300

    update: get latest versions

--- src/kotlin/example/spring/build.gradle.kts
+++ src/kotlin/example/spring/build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.2.4"
-    id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.9.23"
-    kotlin("plugin.spring") version "1.9.23"
+    id("org.springframework.boot") version "3.3.2"
+    id("io.spring.dependency-management") version "1.1.6"
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.spring") version "2.0.0"
 }
 
 group = "com.planetnine"
