f07775b9c2053af142bb2efb71d43d4c00b2d00a
Galacticai <24789792+Galacticai@users.noreply.github.com>
2024-07-28 19:21:16 +0300

    feat: kotlin (purple)

--- src/javascript/example/cli/config/local.js
+++ src/javascript/example/cli/config/local.js
@@ -1,44 +1,50 @@
 const config = {
   colors: {
     blue: {
-      serverURL: 'http://127.0.0.1:3001',
-      signature: 'payload',
-      language: 'javascript'
+      serverURL: "http://127.0.0.1:3001",
+      signature: "payload",
+      language: "javascript",
     },
     green: {
-      serverURL: 'http://127.0.0.1:5000',
-      signature: 'payload',
-      language: 'python'
+      serverURL: "http://127.0.0.1:5000",
+      signature: "payload",
+      language: "python",
     },
     yellow: {
-      serverURL: 'http://127.0.0.1:3002',
-      signature: 'payload',
-      language: 'typescript'
+      serverURL: "http://127.0.0.1:3002",
+      signature: "payload",
+      language: "typescript",
     },
     red: {
-      serverURL: 'http://127.0.0.1:3000', 
-      signature: 'header',
-      language: 'rust'
+      serverURL: "http://127.0.0.1:3000",
+      signature: "header",
+      language: "rust",
     },
-/*    cyan: {
+    /*    cyan: {
       serverURL: 'http://127.0.0.1:8080',
       signature: 'payload',
       language: 'java'
     },*/
     magenta: {
-      serverURL: 'http://127.0.0.1:5139',
-      signature: 'header',
-      language: 'csharp'
-    }
+      serverURL: "http://127.0.0.1:5139",
+      signature: "header",
+      language: "csharp",
+    },
+    purple: {
+      serverURL: "http://localhost:8080",
+      signature: "payload",
+      language: "kotlin",
+    },
   },
   languages: {
-    javascript: 'blue',
-    typescript: 'yellow',
-    rust: 'red',
-    java: 'bgWhite',
-    csharp: 'magenta',
-    python: 'green'
-  }
+    kotlin: "purple",
+    javascript: "blue",
+    typescript: "yellow",
+    rust: "red",
+    java: "bgWhite",
+    csharp: "magenta",
+    python: "green",
+  },
 };
 
 export default config;
