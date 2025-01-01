31373b0ea299fa22fb0719303999419991cfc2a1
zach-planet-nine <zach@planetnine.app>
2024-12-31 17:49:52 -0800

    placeholding some commands

--- src/bash/sessionless.sh
+++ src/bash/sessionless.sh
@@ -2,6 +2,8 @@ openssl ecparam -name secp256k1 -genkey -noout -out private.pem
 
 echo "Your message" > message.txt
 
+keccak-256sum -l message.txt > keccaked
+
 openssl dgst -sha256 -sign private.pem -out signature.bin message.txt
 
 openssl ec -in private.pem -pubout -out public.pem
