23921d8df3e0e3fb841edc1852eda0734fbb4648
zach-planet-nine <zach@planetnine.app>
2025-01-01 10:35:07 -0800

    generate_keys

--- src/bash/sessionless.sh
+++ src/bash/sessionless.sh
@@ -1,11 +1,45 @@
-openssl ecparam -name secp256k1 -genkey -noout -out private.pem
+#!/bin/bash
 
-echo "Your message" > message.txt
+# openssl ecparam -name secp256k1 -genkey -noout -out private.pem
 
-keccak-256sum -l message.txt > keccaked
+# echo "Your message" > message.txt
 
-openssl dgst -sha256 -sign private.pem -out signature.bin message.txt
+# keccak-256sum -l message.txt > keccaked
 
-openssl ec -in private.pem -pubout -out public.pem
+# openssl dgst -sha256 -sign private.pem -out signature.bin message.txt
 
-openssl dgst -sha256 -verify public.pem -signature signature.bin message.txt
+# openssl ec -in private.pem -pubout -out public.pem
+
+# openssl dgst -sha256 -verify public.pem -signature signature.bin message.txt
+
+generate_keys() {
+    local save_keys=$1
+    local get_keys=$2
+
+    openssl ecparam -name secp256k1 -genkey -noout -out private.pem
+    openssl ec -in private.pem -pubout -out public.pem
+}
+
+get_keys() {
+    echo "foo"
+}
+
+sign() {
+    echo "foo"
+}
+
+verify_signature() {
+    echo "foo"
+}
+
+generate_uuid() {
+    echo "foo"
+}
+
+associate_keys() {
+    echo "foo"
+}
+
+if [ "$1" = "generate_keys" ]; then
+    generate_keys $2 $3    
+fi
