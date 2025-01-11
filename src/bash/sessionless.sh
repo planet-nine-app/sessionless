ecb8705b904ebeda048bbbc902376f4d5877801d
zach-planet-nine <zach@planetnine.app>
2025-01-11 09:46:47 -0800

    bash

--- src/bash/sessionless.sh
+++ src/bash/sessionless.sh
@@ -13,27 +13,53 @@
 # openssl dgst -sha256 -verify public.pem -signature signature.bin message.txt
 
 generate_keys() {
-    local save_keys=$1
-    local get_keys=$2
+    if [ -n "$1" && -n "$2" ]; then
+        local private_key_out=$1
+        local public_key_out=$2
+    elif [ -n "$1" ]; then
+        local private_key_out=$1
+        local public_key_out="public.pem"
+    else 
+        local private_key_out="private.pem"
+        local public_key_out="public.pem"
+    fi
 
-    openssl ecparam -name secp256k1 -genkey -noout -out private.pem
-    openssl ec -in private.pem -pubout -out public.pem
+    openssl ecparam -name secp256k1 -genkey -noout -out $private_key_out
+    openssl ec -in private.pem -pubout -out public.pem $public_key_out
 }
 
 get_keys() {
-    echo "foo"
+    if [ -n "$1" && -n "$2" ]; then
+        local private_key_out=$1
+        local public_key_out=$2
+    elif [ -n "$1" ]; then
+        local private_key_out=$1
+        local public_key_out="public.pem"
+    else 
+        local private_key_out="private.pem"
+        local public_key_out="public.pem"
+    fi
+  
+    paste -d "," $private_key_out $public_key_out
 }
 
 sign() {
-    echo "foo"
+    local message="$1"
+    local message_hash=$(echo keccak-256sum -l $message)
+
+    openssl pkeyutl -sign -inkey private.pem -in <(echo -n "$keccaked") -out signature.bin -pkeyopt digest:none -pkeyopt ec_scheme:sm2
 }
 
 verify_signature() {
-    echo "foo"
+    local signature=$1
+    local message=$2
+    local pubKey=$3
+
+    openssl -verify public.pem -signature $signature $message 
 }
 
 generate_uuid() {
-    echo "foo"
+    openssl rand -hex 16 | awk '{print substr($0,1,8) "-" substr($0,9,4) "-4" substr($0,14,3) "-" substr($0,17,4) "-" substr($0,21)}' > uuid
 }
 
 associate_keys() {
@@ -42,4 +68,6 @@ associate_keys() {
 
 if [ "$1" = "generate_keys" ]; then
     generate_keys $2 $3    
+elif [ "$1" = "generate_uuid" ]; then
+    generate_uuid
 fi
